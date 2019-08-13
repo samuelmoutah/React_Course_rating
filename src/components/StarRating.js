90169605a5603ba70995d4584c7b4944c5a9077d
samuelmo <samuelmo78@gmail.com>
2019-08-13 12:23:02 -0600

    first commit

---
+++ src/components/StarRating.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Star from './Star';
+
+class StarRating extends Component {
+
+  // Initialize a 'rating' state
+  state = {
+    rating: 0
+  }
+
+  // Write a function that returns 5 Star components
+  renderStars = () => {
+    let stars = []
+    let maxRating = 5
+  
+
+    for(let i=0; i < maxRating; i++) {
+      stars.push(
+        <Star
+          isSelected={this.state.rating > i}
+          setRating={() => this.handleSetRating(i + 1) } 
+          key={i}
+        />
+      )
+    }
+    return stars
+  }
+
+  // Write an event handler that updates the rating state.
+  // Pass the function to a Star component via props
+
+  handleSetRating = (rating) => {
+    if(this.state.rating === rating) {
+      this.setState({ rating: 0})
+    } else {
+      this.setState({
+        rating: rating
+      })
+    }
+    
+  }
+
+  render() {
+    return (
+      <ul className="course--stars">
+        {/* Render the Star components */}
+        { this.renderStars() }
+      </ul>
+    );
+  }
+}
+
+export default StarRating;
